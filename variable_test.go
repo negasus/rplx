fd47358220792f0af8451b8372ad649ae682f935
Negasus <negasus@gmail.com>
2019-09-30 06:56:17 +0300

    add test for variable.TTLVersion

--- variable_test.go
+++ variable_test.go
@@ -132,3 +132,44 @@ func TestVariableTTL(t *testing.T) {
 		})
 	}
 }
+
+func TestVariableTTLVersion(t *testing.T) {
+	type fields struct {
+		name        string
+		self        *variableItem
+		ttl         int64
+		ttlVersion  int64
+		remoteItems map[string]*variableItem
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int64
+	}{
+		{
+			name: "get ttl version",
+			fields: fields{
+				name:        "var1",
+				self:        &variableItem{},
+				ttl:         0,
+				ttlVersion:  200,
+				remoteItems: map[string]*variableItem{},
+			},
+			want: 200,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := &variable{
+				name:        tt.fields.name,
+				self:        tt.fields.self,
+				ttl:         tt.fields.ttl,
+				ttlVersion:  tt.fields.ttlVersion,
+				remoteItems: tt.fields.remoteItems,
+			}
+			if got := v.TTLVersion(); got != tt.want {
+				t.Errorf("TTLVersion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
