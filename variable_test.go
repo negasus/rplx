07f355a106badf8786725f1d45657f4cceae8295
Negasus <negasus@gmail.com>
2019-09-30 06:53:05 +0300

    add test for variable.Get

--- variable_test.go
+++ variable_test.go
@@ -36,3 +36,58 @@ func TestVariableNew(t *testing.T) {
 		})
 	}
 }
+
+func TestVariableGet(t *testing.T) {
+	type fields struct {
+		name        string
+		self        *variableItem
+		ttl         int64
+		ttlVersion  int64
+		remoteItems map[string]*variableItem
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int64
+	}{
+		{
+			name: "get value with only self data",
+			fields: fields{
+				name:        "var1",
+				self:        &variableItem{val: 100},
+				ttl:         0,
+				ttlVersion:  0,
+				remoteItems: map[string]*variableItem{},
+			},
+			want: 100,
+		},
+		{
+			name: "get value with self and remote data",
+			fields: fields{
+				name:       "var1",
+				self:       &variableItem{val: 100},
+				ttl:        0,
+				ttlVersion: 0,
+				remoteItems: map[string]*variableItem{
+					"node2": {val: 200},
+					"node3": {val: 300},
+				},
+			},
+			want: 600,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := &variable{
+				name:        tt.fields.name,
+				self:        tt.fields.self,
+				ttl:         tt.fields.ttl,
+				ttlVersion:  tt.fields.ttlVersion,
+				remoteItems: tt.fields.remoteItems,
+			}
+			if got := v.get(); got != tt.want {
+				t.Errorf("get() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
